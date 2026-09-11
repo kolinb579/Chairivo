129c2848523e110af9c8127cb4def9dc9cd97c51
kolinb579 <kolinb579@gmail.com>
2026-09-11 23:04:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	
-	<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-BVNCJQL93V"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WR1JP3WH5L"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-BVNCJQL93V');
+  gtag('config', 'G-WR1JP3WH5L');
 </script>
+	
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Chairivo · premium office chairs</title>
