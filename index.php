a4f4688d0b93db5540e8e44bed2ebcdce09ee115
kolinb579 <kolinb579@gmail.com>
2026-09-11 20:29:03 +0530

    Remove web analytics script from index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<script src="https://app.zerocloak.com/web-analytics.js/mjuon6zgbm"></script>
+	
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Chairivo · premium office chairs</title>
